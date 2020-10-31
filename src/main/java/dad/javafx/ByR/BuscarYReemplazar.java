636074cb7aa291d5f4187b17fb4d280863c1ecb0
Tevu-W <55486791+Tevu-W@users.noreply.github.com>
2020-10-31 20:30:02 +0000

    Update BuscarYReemplazar.java

--- src/main/java/dad/javafx/ByR/BuscarYReemplazar.java
+++ src/main/java/dad/javafx/ByR/BuscarYReemplazar.java
@@ -9,8 +9,11 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -51,18 +54,51 @@ public class BuscarYReemplazar extends Application{
 		cajas.add(atras, 1, 0);
 		cajas.add(resultados, 1, 1);
 		
+		
+		VBox botones = new VBox();
+		botones.setPrefWidth(110);
+		botones.setAlignment(Pos.CENTER);
+		botones.setSpacing(5);
+		BuscarButton.setMinWidth(botones.getPrefWidth());
+		ReemplazarButton.setMinWidth(botones.getPrefWidth());
+		TodoButton.setMinWidth(botones.getPrefWidth());
+		CerrarButton.setMinWidth(botones.getPrefWidth());
+		AyudaButton.setMinWidth(botones.getPrefWidth());
+		botones.getChildren().addAll(BuscarButton, ReemplazarButton, TodoButton, CerrarButton, AyudaButton);
+		
+		
 		GridPane root = new GridPane();
 		root.setPadding(new Insets(5));
 		root.setHgap(5);
 		root.setVgap(5);
-		root.setGridLinesVisible(true);
+		//root.setGridLinesVisible(true);
 		root.addRow(0, BuscarLabel);
 		root.addRow(1, ReemplazarLabel);
 		root.addColumn(1, BuscarField);
 		root.addColumn(1, ReemplazarField);
 		root.add(cajas, 1, 2);
+		root.addColumn(2, botones);
 		
-		Scene scene = new Scene(root, 640, 480);
+		ColumnConstraints [] cols = {
+				new ColumnConstraints(),
+				new ColumnConstraints(),
+				new ColumnConstraints(),
+		};
+		root.getColumnConstraints().setAll(cols);
+		
+		cols[1].setHgrow(Priority.ALWAYS);
+		
+		RowConstraints [] rows = {
+				new RowConstraints(), 
+				new RowConstraints(), 
+				new RowConstraints(),
+		};
+		root.getRowConstraints().setAll(rows);
+		
+		root.setRowSpan(botones, 3);
+		
+		
+		Scene scene = new Scene(root, 550, 370);
 		
 		primaryStage.setTitle("Buscar Y Reemplazar");
 		primaryStage.setScene(scene);
