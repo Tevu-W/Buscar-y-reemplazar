4661f275392aa936fed196e29b858ccd98ce88fa
Tevu-W <55486791+Tevu-W@users.noreply.github.com>
2020-10-31 18:29:38 +0000

    Update BuscarYReemplazar.java

--- src/main/java/dad/javafx/ByR/BuscarYReemplazar.java
+++ src/main/java/dad/javafx/ByR/BuscarYReemplazar.java
@@ -1,5 +1,19 @@
 package dad.javafx.ByR;
 
-public class BuscarYReemplazar {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BuscarYReemplazar extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
